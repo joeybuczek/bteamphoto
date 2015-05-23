6ebc5e5ac3dfcd9aac8e78ec74f7218478c2d6d4
Joey Buczek <joe.buczek@gmail.com>
2015-05-23 04:30:41 +0000

    updated url for mailer

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -5,8 +5,7 @@ class InvitationMailer < ActionMailer::Base
     @user = user
     @temporary_password = temporary_password
     @token = @user.invitations.last.token
-    # @url = "http://localhost:3000/users/confirmation?confirmation_token=krjNpNTsA-KjyrZs7YQH"
-    @url = "http://localhost:3000/confirmation?token=#{@token}"
+    @url = "https://bteamphoto.herokuapp.com/confirmation?token=#{@token}"
     mail(to: @user.email, subject: 'B-Team Photography Account Setup Confirmation')
   end
   
