4cff8ca5ef59213e575c2edf14cb1b922939dc48
Joey Buczek <joe.buczek@gmail.com>
2015-06-01 03:15:16 +0000

    updated views with money

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,9 +1,5 @@
 class Invoice < ActiveRecord::Base
   belongs_to :invoiceable, polymorphic: true
-  attr_reader :balance_cents, :balance_currency
-  
-  def initialize
-    @balance_currency = "USD"
-  end
+  monetize :balance_cents
   
 end
