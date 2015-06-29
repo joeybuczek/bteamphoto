8553391a82b75beb781c814fa605d448e624089f
Joey Buczek <joe.buczek@gmail.com>
2015-06-28 22:26:24 -0400

    integrating angularjs

--- app/assets/javascripts/bteamphoto.js
+++ app/assets/javascripts/bteamphoto.js
@@ -1,1 +1,12 @@
-// BTeamPhoto JS
+// bteamphoto angularjs app code
+
+angular.module('reviewsApp', [])
+    .controller('ReviewsController', [function() {
+      var self = this;
+      self.notes = [
+        { id: 1, label: 'First Note', done: false },
+        { id: 2, label: 'Second Note', done: false },
+        { id: 3, label: 'Done Note', done: true },
+        { id: 4, label: 'Last Note', done: false }
+      ];
+  }]);
