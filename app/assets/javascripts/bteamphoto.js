70aee0c2d1b9ed4aa36553104021ef9d4aee3979
Joey Buczek <joe.buczek@gmail.com>
2015-06-28 22:48:59 -0400

    basic angular test with deployment

--- app/assets/javascripts/bteamphoto.js
+++ app/assets/javascripts/bteamphoto.js
@@ -2,11 +2,5 @@
 
 angular.module('reviewsApp', [])
     .controller('ReviewsController', [function() {
-      var self = this;
-      self.notes = [
-        { id: 1, label: 'First Note', done: false },
-        { id: 2, label: 'Second Note', done: false },
-        { id: 3, label: 'Done Note', done: true },
-        { id: 4, label: 'Last Note', done: false }
-      ];
-  }]);
+      
+}]);
